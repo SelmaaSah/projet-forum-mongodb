6ae2f11470944d9cdbea4d56ac68d5ff60df538f
SelmaaSah <144896543+SelmaaSah@users.noreply.github.com>
2026-01-11 18:00:43 +0100

    Update header.php

---
+++ views/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Forum MongoDB</title>
+    <link rel="stylesheet" href="assets/style.css"> 
+</head>
+<body>
+    <header>
+        <h1><a href="index.php">Forum CFA des Sciences</a></h1>
+        <nav>
+            <a href="index.php?action=register">Inscription</a>
+            <a href="index.php?action=login">Connexion</a>
+        </nav>
+        <hr>
+    </header>
+    <main>
