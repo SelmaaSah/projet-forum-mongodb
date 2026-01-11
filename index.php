d82d92f1beef6e46d82afecd1fb48f7374824e8a
SelmaaSah <144896543+SelmaaSah@users.noreply.github.com>
2026-01-11 18:07:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <?php
-session_start(); 
+// index.php
+session_start();
 require_once 'config/Manager.php';
 
-echo "<h1> Projet Forum</h1>";
+$action = isset($_GET['action']) ? $_GET['action'] : 'list';
 
-try {
-    $test = new Manager();
-    echo "<p> Connexion à MongoDB réussie </p>";
-} catch (Exception $e) {
-    echo "<p> Erreur de connexion : " . $e->getMessage() . "</p>";
+switch($action) {
+    
+    case 'register':
+        require_once 'controllers/Users.php';
+        $controller = new UsersController();
+        $controller->register();
+        break;
+
+    case 'login':
+        require_once 'controllers/Users.php';
+        $controller = new UsersController();
+        $controller->login();
+        break;
+
+    //  Déconnexion ---
+    case 'logout':
+        require_once 'controllers/Users.php';
+        $controller = new UsersController();
+        $controller->logout();
+        break;
+
+    default:
+        require 'views/partials/header.php';
+        
+        if (isset($_SESSION['pseudo'])) {
+            echo "<h2>Bonjour " . $_SESSION['pseudo'] . " !</h2>";
+            echo "<p>Vous êtes connecté.</p>";
+            echo "<a href='index.php?action=logout'>Se déconnecter</a>";
+        } else {
+            echo "<h2>Bienvenue Visiteur</h2>";
+            echo "<p>Veuillez vous <a href='index.php?action=login'>connecter</a>.</p>";
+        }
+        
+        require 'views/partials/footer.php';
+        break;
 }
 ?>
