06e0746a919690e7c486bab4faea0d0d01ae9102
SelmaaSah <144896543+SelmaaSah@users.noreply.github.com>
2026-01-11 17:09:24 +0100

    Update Manager.php

---
+++ config/Manager.php
@@ -0,0 +1,29 @@
+<?php
+
+class Manager {
+    protected $manager;
+    protected $dbName = "bdd-forum"; 
+
+    public function __construct() {
+        // connexion a la bdd
+        try {
+            $this->manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+        } catch (Exception $e) {
+            die("Erreur de connexion à MongoDB : " . $e->getMessage());
+        }
+    }
+
+    protected function executeQuery($collection, $filter = [], $options = []) {
+        $query = new MongoDB\Driver\Query($filter, $options);
+        $cursor = $this->manager->executeQuery($this->dbName.'.'.$collection, $query);
+        return $cursor->toArray();
+    }
+
+    protected function insertOne($collection, $document) {
+        $bulk = new MongoDB\Driver\BulkWrite;
+        $id = $bulk->insert($document);
+        $this->manager->executeBulkWrite($this->dbName.'.'.$collection, $bulk);
+        return $id;
+    }
+}
+?>
